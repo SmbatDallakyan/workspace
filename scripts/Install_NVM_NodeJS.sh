a5bdde50eee70d741c8e58c8b2a50215d21e2939
Smbat Dallakyan <smbat.dallakyan@gmail.com>
2017-11-15 15:04:18 +0400

    Added scripts for install NodeJS, NVM and VPN

---
+++ scripts/Install_NVM_NodeJS.sh
@@ -0,0 +1,9 @@
+sudo apt-get update
+sudo apt-get install build-essential libssl-dev
+curl https://raw.githubusercontent.com/creationix/nvm/v0.25.0/install.sh | bash
+source ~/.profile
+nvm --version
+
+nvm install 6.9.4
+nvm current
+nvm use 6.9.4
