e36cac08d6f7f6df69730a64c540e902ad3648e2
SmbatDallakyan <smbat.dallakyan@gmail.com>
2017-11-17 14:43:48 +0400

    Update Install_NVM_NodeJS.sh

--- scripts/Install_NVM_NodeJS.sh
+++ scripts/Install_NVM_NodeJS.sh
@@ -2,8 +2,8 @@ sudo apt-get update
 sudo apt-get install build-essential libssl-dev
 curl https://raw.githubusercontent.com/creationix/nvm/v0.25.0/install.sh | bash
 source ~/.profile
-nvm --version
 
 nvm install 6.9.4
 nvm current
 nvm alias default 6.9.4
+nvm use 6.9.4
